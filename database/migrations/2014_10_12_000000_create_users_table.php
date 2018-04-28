946e4b8650b0f7a63996f25a09c0e0bf77b05562
Alejandro <fer.ale.mkt@gmail.com>
2018-04-28 12:26:06 -0500

    Iniciando el proyecto

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('admin')->default(0);
+            $table->string('name');
+            $table->string('lastname');
+            $table->string('company');
+            $table->string('phone');
+            $table->string('country');
+            $table->integer('postal_code');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->boolean('status')->default(1);
+            $table->boolean('optcontract')->nullable();
+            $table->string('braintree_id')->nullable();
+            $table->string('paypal_email')->nullable();
+            $table->string('card_brand')->nullable();
+            $table->string('card_last_four')->nullable();            
+            $table->timestamp('trial_ends_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
